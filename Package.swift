6e210b022f84eac08a70945979e24c55b04e8504
jianbing li <ljb_wh@hotmail.com>
2021-12-16 19:51:50 +1300

    use hub.fastgit.org

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mapbox",
-            url: "https://github.com/maplibre/maplibre-gl-native/releases/download/ios-v5.12.2/Mapbox-5.12.2.zip",
+            url: "https://hub.fastgit.org/maplibre/maplibre-gl-native/releases/download/ios-v5.12.2/Mapbox-5.12.2.zip",
             checksum: "348453495bad198b380e033a6bc6e6134bf71be27116904a25152b93a2d77491")
     ]
 )
